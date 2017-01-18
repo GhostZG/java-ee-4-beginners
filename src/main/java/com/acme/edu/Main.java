7dd530c4cca3b02109e7c18722cd00064568d722
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2017-01-18 13:41:45 +0300

    Annotations demo.

--- src/main/java/com/acme/edu/Main.java
+++ src/main/java/com/acme/edu/Main.java
@@ -4,25 +4,15 @@
  * and open the template in the editor.
  */
 package com.acme.edu;
+
 /**
  *
  * @author eugene
  */
+
+@SuppressWarnings({"", ""})
 public class Main {
-    public static void main(String[] args) throws ClassNotFoundException {
-        //1. Introspection
-        String obj = new String();
-        obj.getClass();        
-        Main.m(String.class);
+    public static void main(String[] args) {
         
-        //2. Dynamic Class loading
-        Class.forName("Main22222");
-                
-        //3. Dynamic/Virtual Proxies
-        //4. Byte-code manipulation / instrumenting : ByteBuddy, ASM (AST)
-    }
-    
-    static <T> T m(Class<T> a) {
-        return null;
     }
 }
