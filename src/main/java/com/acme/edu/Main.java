f1942e57d1d9b6d003a4afebf29723c607fd5ca9
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2017-01-19 14:48:14 +0300

    State scopes Demo.

--- src/main/java/com/acme/edu/Main.java
+++ src/main/java/com/acme/edu/Main.java
@@ -1,91 +1,46 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.acme.edu;
 
-import java.io.Closeable;
+import javax.annotation.PostConstruct;
 import java.io.IOException;
-import java.util.*;
-
-import static java.util.Collections.emptyList;
-
-/**
- *
- * @author eugene
- */
+import java.io.InputStream;
 
 public class Main {
     public static void main(String[] args) {
-//        Collections.sort();
-        Collections.unmodifiableList(Arrays.asList(1, 2, 3, 0));
-//        Collections.emptyList().add("");
-        Collections.synchronizedList(emptyList());
+        Controller c = new Controller(
+            new XXxWriter(),
+            new XxxReader()
+        );
+        c.doEtl();
+        c.doEtl();
 
-        //Main$1.class
-        Closeable cc = new Closeable() {
-            @Override
-            public void close() throws IOException {
+        //Scope: request, session, aaplication, ....
+        c = new Controller(
+            new XXxWriter(),
+            new XxxReader()
+        );
+        c.doEtl();
 
-            }
-        };
-
-        //1.
-        Set init = new HashSet() {{
-
-        }};
-
-        //2.
-        Set init2 = map(1,2, set());
-
-
-        Set collision = new HashSet();
-        DTO dto1 = new DTO(1);
-        collision.add(dto1);
-        dto1.setState(2);
-
-        collision.add(new DTO(2));
-        System.out.println(collision);
-
+        c = new Controller() {
+            new xxxWriter(),
+            new ReaderFactory(),
+        }
 
     }
 }
 
-class Anon implements Closeable {
-    @Override
-    public void close() throws IOException {
+@Component
+@Bean
+@Scope
+class Controller {
+    @Inject @Named("") private Writer eriter;
+    @Autowired("ds_name") private DataSrouce ds;
 
-    }
+    @PostConstruct
+
 }
 
-class DTO {
-    private int state;
-
-
-
-    DTO(int state) {
-//        super();
-        this.state = state;
-    }
-
-    public int getState() {
-        return state;
-    }
-
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof DTO)) return false;
-
-        DTO dto = (DTO) o;
-
-        return getState() == dto.getState();
-    }
-
-    @Override
-    public int hashCode() {
-        return getState();
-    }
-}
+<beans>
+    <bean id = "jhdkg" type =CsvWrter>
+        <constructor-arg>3
+    </b>
+</b>
