1e2dbc3a59a81aa63194d840bb6a192a2c337eae
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2017-01-19 15:35:05 +0300

    Annotation Intro.

--- src/main/java/com/acme/edu/Main.java
+++ src/main/java/com/acme/edu/Main.java
@@ -1,10 +1,14 @@
 package com.acme.edu;
 
 import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Date;
+
 
 public class Main {
+    @SuppressWarnings("jhkdjfhg")
     public static void main(String[] args) throws ClassNotFoundException {
         //1. Introspection
         Object o = new Object();
@@ -20,5 +24,13 @@ public class Main {
         //4. Dynamic/Virtual Proxy: Interface + Code -> Byte-code Instrumenting
 
         //5. Byte-code manipulation: AST -> meta-programming: ASM | ByteBuddy
+    }
+}
+
+@Component
+class MyService {
+    @javax.annotation.PreDestroy
+    public void m() {
+        //clean-up
     }
 }
