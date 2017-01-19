e3f764d43a2079377f192fb93382fbc21762c904
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2017-01-19 17:51:55 +0300

    JDBC Demo.

--- src/main/java/com/acme/edu/Main.java
+++ src/main/java/com/acme/edu/Main.java
@@ -1,15 +1,49 @@
 package com.acme.edu;
 
-import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Date;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Main {
-    public static void main(String[] args) throws ClassNotFoundException {
-        System.load(""); //JNI -> JNA -> jCOM
-        final Process exec = Runtime.getRuntime().exec("util -kfjghdkfjgh ");
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        //Driver Types: 1 (jdbc-odbc bridge), 2 (native), 3 (network), 4 (pure java)
+//        Class.forName("driver class");
+        try(Connection connection = 
+                DriverManager.getConnection("jdbc:derby://localhost/example;create=true", "APP", "APP")) {
+                Statement st = connection.createStatement(ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_UPDATABLE);
+                
+                connection.setTransactionIsolation(Connection.TRANSACTION_READ_UNCOMMITTED);
+                connection.getWarnings();
+                connection.setAutoCommit(false);
+//                st.executeUpdate("INSERT INTO USERS VALUES(9,'9')");
+                PreparedStatement psmt = connection.prepareStatement("INSERT INTO USERS VALUES(?,?)");
+                psmt.setInt(1, 8);
+                psmt.setString(2, "8");
+                psmt.executeUpdate();
+                psmt.getWarnings();
+                
+                ResultSet rs = st.executeQuery("SELECT * FROM USERS");
+                while(rs.next()) {
+//                    rs.previous();
+                    System.out.println(
+                        rs.getInt(1) + ":" +
+                        rs.getString("NAME")
+                    );
+//                    rs.updateInt("name", 3);
+//                    rs.updateRow();
+                }
+                connection.commit();
+        }
+    }
+}
+
+class MyDriver {
+    static  {
+//        DriverManager.registerDriver(new MyDriver());
     }
 }
