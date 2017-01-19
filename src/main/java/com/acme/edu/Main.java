dc286138594f06da9487f5f73e3e33053a57b5b0
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2017-01-19 15:51:11 +0300

    JNI Demo.

--- src/main/java/com/acme/edu/Main.java
+++ src/main/java/com/acme/edu/Main.java
@@ -8,29 +8,8 @@ import java.util.Date;
 
 
 public class Main {
-    @SuppressWarnings("jhkdjfhg")
     public static void main(String[] args) throws ClassNotFoundException {
-        //1. Introspection
-        Object o = new Object();
-        o.getClass();
-        Class clazz = Object.class;
-
-        //2. Actions
-//        clazz.newInstance();
-
-        //3. Dynamic classloading
-        Class.forName("p.p.p.C");
-
-        //4. Dynamic/Virtual Proxy: Interface + Code -> Byte-code Instrumenting
-
-        //5. Byte-code manipulation: AST -> meta-programming: ASM | ByteBuddy
-    }
-}
-
-@Component
-class MyService {
-    @javax.annotation.PreDestroy
-    public void m() {
-        //clean-up
+        System.load(""); //JNI -> JNA -> jCOM
+        final Process exec = Runtime.getRuntime().exec("util -kfjghdkfjgh ");
     }
 }
